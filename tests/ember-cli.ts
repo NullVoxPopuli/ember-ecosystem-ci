8360a026b685ed60dcfc7d6a51698cc723b832ea
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-02-17 15:06:57 -0500

    Better output

--- tests/ember-cli.ts
+++ tests/ember-cli.ts
@@ -1,17 +1,58 @@
 import { prepare } from '#utils';
 import { execaCommand } from 'execa';
 import { join } from 'node:path';
+import { styleText } from 'node:util';
 
 let { cli } = await prepare({ cli: true, source: false });
 
 const [, , ...args] = process.argv;
 const bin = join(cli.dir, 'bin', 'ember');
 
-const command = `${bin} new my-app ${args.join(' ')}`
+const command = `${bin} new my-app ${args.join(' ')} --skip-install`
 
 await execaCommand(command, { cwd: 'tmp' });
 
-await execaCommand(`npm run lint`, { cwd: 'tmp/my-app' })
-await execaCommand(`npm run lint:fix`, { cwd: 'tmp/my-app' })
-await execaCommand(`npm run test`, { cwd: 'tmp/my-app' })
-await execaCommand(`npm run build`, { cwd: 'tmp/my-app' })
+
+async function run(cmd: string) {
+  let promise = execaCommand(cmd, { cwd: 'tmp/my-app' });
+
+  try {
+    let result = await promise;
+
+    return result.exitCode === 0;
+  } catch (e) {
+    return false;
+  }
+}
+
+let manager = command.includes('--pnpm') ? 'pnpm' : 'npm';
+
+let install = await run(`${manager} install`);
+let lint = await run(`${manager} run lint`)
+let lintFix = await run(`${manager} run lint:fix`)
+let test = await run(`${manager} run test:ember`)
+let prodbuild = await run(`${manager} run build`)
+
+
+let isSuccess = [lint, lintFix, test, prodbuild].every(Boolean);
+
+function pf(bool: boolean) {
+  return bool ? 'pass' : 'fail';
+}
+
+console.info(`
+
+  install    ${pf(install)}
+  lint       ${pf(lint)}
+  lint:fix   ${pf(lintFix)}
+  test:ember ${pf(test)}
+  build      ${pf(prodbuild)}
+
+  Overall: ${isSuccess}
+`);
+
+if (!isSuccess) {
+  process.exit(1);
+}
+
+
