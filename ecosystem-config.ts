16c95a651bcbf3883d988528623b17cc114b4026
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-02-17 16:37:22 -0500

    Try this

--- ecosystem-config.ts
+++ ecosystem-config.ts
@@ -1,9 +1,14 @@
 interface Entry {
   // This will show up in the CI matrix
   name: string;
+  // git URL to checkout the code
   repo: string;
+  // What to do for install, any needed prebuild, etc
   setup: string;
+  // Comamnd to run for testing, must exit with status code 0 to indicate success
   test: string;
+  // Optionally directory to run the 'test' command in.
+  testDir?: string;
 }
 export const config: Entry[] = [
   {
@@ -13,6 +18,10 @@ export const config: Entry[] = [
     test: 'pnpm test:ember'
   },
   {
+    name: 'ember-resources',
+    repo: 'https://github.com/NullVoxPopuli/ember-resources.git',
+    setup: 'pnpm install; pnpm build',
+    testDir: 'test-app',
+    test: 'pnpm test:ember'
   }
-
 ]
